9c28372158d54e5f2abc25a080d74c55a089e770
Aang Ardiansyah-XD <92802033+AngCyber@users.noreply.github.com>
2021-11-14 20:51:41 +0700

    Add files via upload

---
+++ a.py
@@ -0,0 +1,17 @@
+import sys, os
+
+if sys.version[0:3] != "3.9":
+  sys.exit("):
+  	print '\n\n %s[•]%s Selamat Datang -> [ %s%s%s ]'%(O,N,K,nama,N);time.sleep(2)
+        print ' %s[•]%s Selamat menggunakan script saya'%(O,N);time.sleep(2)
+        print ' %s[•]%s Bismillah dulu biar dapet OK, karna modal hoki doang'%(O,N);time.sleep(2)
+        print ' %s[•]%s Gak bisa bikin akun OK!!'%(O,N);time.sleep(2)
+        print ' %s[•]%s Biasa Aja Kentod, Gak Usah Desahh_-'%(O,N);time.sleep(2)
+        open('.memek.txt', 'w').write(kontol)
+
+if __name__ == "__main__":
+  try:
+      #os.system("git pull")
+      __import__("Brute").cek()
+  except Exception as e:
+    exit(str(e))
