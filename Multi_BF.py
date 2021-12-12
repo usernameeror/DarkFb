feee50d7b74402fe67a8bc6b3c6d710b95bde382
Aang Ardiansyah-XD <92802033+AngCyber@users.noreply.github.com>
2021-12-12 23:45:36 +0700

    Add files via upload

--- Multi_BF.py
+++ Multi_BF.py
@@ -74,7 +74,7 @@ url = "https://mbasic.facebook.com"
 hoetank = random.choice(['Yang posting orang nya ganteng:)', 'Lo ngentod:v', 'Never surrentod tekentod kentod:v'])
 bulan_ttl = {"01": "Januari", "02": "Februari", "03": "Maret", "04": "April", "05": "Mei", "06": "Juni", "07": "Juli", "08": "Agustus", "09": "September", "10": "Oktober", "11": "November", "12": "Desember"}
 
-# lempankkkkkkkk
+# Aang Gantenkkkkkk
 def jalan(z):
     for e in z + '\n':
         sys.stdout.write(e)
